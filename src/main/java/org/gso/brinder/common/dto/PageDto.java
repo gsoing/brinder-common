fe88a6385d4e41bc182b27d110596400bb09e2d8
Guillaume Soing <guillaume.soing@renault.com>
2021-09-16 11:42:52 +0200

    add spring autoconfiguration

--- src/main/java/org/gso/brinder/common/dto/PageDto.java
+++ src/main/java/org/gso/brinder/common/dto/PageDto.java
@@ -1,14 +1,20 @@
 package org.gso.brinder.common.dto;
 
 import java.net.URI;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
-public abstract class PageDto {
+/**
+ * DTO to expose paginable results
+ */
+public class PageDto<T> {
 
     private int pageSize;
-    private int totalElements;
+    private long totalElements;
     private URI next;
-
+    private URI first;
+    private URI last;
+    private List<T> data;
 }
